6d434ebbae4bd070a95fe3539064b5bc6b60e34b
Kacper Sagnowski <ksagnowski@gmail.com>
2018-12-20 12:27:41 +0100

    Improve onset detection accuracy

--- detectOnsets.m
+++ detectOnsets.m
@@ -1,10 +1,10 @@
 function onsetLocs = detectOnsets(audioIn, minPeakDist)
-    fftSize = 2048;
+    fftSize = 64;  % Keep this low to maintain high accuracy
     hopSize = 64;
 
     cursor = 1;
     novelty = zeros(length(audioIn), 1);
-    previousFrameFFT = zeros(fftSize, 1);
+    previousFrameFFT = fft(audioIn(1:fftSize) .* hann(fftSize));
 
     wb = waitbar(0, 'Detecting onsets...');
     while cursor + fftSize < length(audioIn)
@@ -12,25 +12,22 @@ function onsetLocs = detectOnsets(audioIn, minPeakDist)
         frameFFT = fft(audioIn(cursor:cursor + fftSize - 1) .* hann(fftSize));
         fftDifference = abs(frameFFT(1:fftSize / 2 - 1)) - abs(previousFrameFFT(1:fftSize / 2 - 1));
 
-        % novelty(cursor:cursor + hopSize - 1) = sqrt(sum(fftDifference.^2)) / (fftSize / 2);
         novelty(cursor:cursor + hopSize - 1) = sum(fftDifference); % Keeping the sign of the difference helps differentiate between note on and note off
         previousFrameFFT = frameFFT;
         cursor = cursor + hopSize;
     end
 
     waitbar(0.9, wb);
-    fadeIn = linspace(0, 1, minPeakDist)';
-    novelty(1:minPeakDist) = novelty(1:minPeakDist) .* fadeIn;
+
     maxVal = max(novelty);
     novelty = novelty / maxVal;
     [~, onsetLocs] = findpeaks(novelty, 'MinPeakProminence', 0.5, 'MinPeakDistance', minPeakDist);
 
     close(wb);
 
-    % subplot(2, 1, 1)
-    % plot(audioIn)
-    % subplot(2, 1, 2)
-    % plot(novelty)
-    % hold
-    % plot(onsetLocs, onsetPeaks, 'x')
+    % plot(audioIn);
+    % hold;
+    % plot(novelty);
+    % plot(onsetLocs, onsetPeaks, 'x');
+    % hold;
 end
--- main.m
+++ main.m
@@ -3,7 +3,8 @@ close all;
 
 %% User settings
 tempo = 180;
-duration = 5;
+duration = 10;
+subtractAverageLag = true;
 
 %% Technical settings
 fs = 41000;
@@ -12,8 +13,8 @@ frameLength = 256;
 %% Setup system
 deviceReader = audioDeviceReader(fs, frameLength);
 setup(deviceReader);
-% lag = measureAudioLag(deviceReader);
-% lag = 7200;
+% averageLag = measureAudioLag(deviceReader);
+% averageLag = 7200;
 
 %% Setup session
 [metronome, tickArray] = generateMetronome(tempo, duration, fs);
@@ -25,22 +26,26 @@ audioIn = recordAudioIn(duration, deviceReader);
 
 %% Process
 audioIn = highpass(audioIn, 50, fs);
+
 minTickDist = getTickDistance(tempo, fs) / 2;
 onsetLocs = detectOnsets(audioIn, minTickDist);
 tickLocs = getTickLocs(tickArray);
 timingInfo = getTimingInfo(onsetLocs, tickLocs);
-lag = round(timingInfo.average);
-metronome = circshift(metronome, lag);
-metronome = metronome(1:length(audioIn));
-tickArray = circshift(tickArray, lag);
-tickLocs = getTickLocs(tickArray);
+
+if subtractAverageLag
+    averageLag = round(timingInfo.average);
+    tickArrayAligned = circshift(tickArray, averageLag);
+    tickLocs = getTickLocs(tickArrayAligned);
+else
+    tickLocs = getTickLocs(tickArray);
+end
 
 %% Plot
-subplot(2, 1, 1);
-plot(metronome);
-subplot(2, 1, 2);
-plot(audioIn);
+time = linspace(0, length(audioIn)/fs, length(audioIn));
+plot(time, audioIn);
 hold;
-plot(onsetLocs, zeros(length(onsetLocs)), 'x', 'LineWidth', 2, 'MarkerSize', 10, 'Color', 'r');
-plot(tickLocs, zeros(length(tickLocs)), '+', 'LineWidth', 2, 'MarkerSize', 10, 'Color', 'g');
+onsetTimes = onsetLocs / fs;
+plot(onsetTimes, zeros(length(onsetTimes)), 'x', 'LineWidth', 2, 'MarkerSize', 10, 'Color', 'r');
+tickTimes = tickLocs / fs;
+plot(tickTimes, zeros(length(tickTimes)), '+', 'LineWidth', 2, 'MarkerSize', 10, 'Color', 'g');
 hold;
