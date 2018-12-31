cac15485b62040c89b01afc3c60566a915cb482b
Kacper Sagnowski <ksagnowski@gmail.com>
2018-12-31 12:10:46 +0100

    Add CircularBuffer class

---
+++ CircularBuffer.m
@@ -0,0 +1,43 @@
+classdef CircularBuffer < handle
+    properties
+        data;
+        previousData;
+        bufferSize;
+    end
+
+    properties (Access = private)
+        cursor;
+    end
+
+    methods
+
+        function self = CircularBuffer(bufferSize)
+            self.bufferSize = bufferSize;
+            self.data = zeros(bufferSize, 1);
+            self.previousData = zeros(bufferSize, 1);
+            self.cursor = 1;
+        end
+
+        function filled = add(self, newData)
+            dataLen = length(newData);
+
+            if self.cursor + dataLen > self.bufferSize
+                numToBufferEnd = self.bufferSize - self.cursor + 1;
+                numRemaining = length(newData) - numToBufferEnd;
+                self.data(self.cursor:self.cursor + numToBufferEnd - 1) = newData(1:numToBufferEnd);
+                self.previousData = self.data;
+                self.data = zeros(self.bufferSize, 1);
+                self.data(1:numRemaining) = newData(numToBufferEnd + 1:end);
+                self.cursor = 1;
+                filled = true;
+            else
+                self.data(self.cursor:self.cursor + dataLen - 1) = newData;
+                self.cursor = self.cursor + dataLen;
+                filled = false;
+            end
+
+        end
+
+    end
+
+end
