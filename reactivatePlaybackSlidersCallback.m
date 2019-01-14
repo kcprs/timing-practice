3734855523cf1264da42610bbf481eb8c19e1f07
Kacper Sagnowski <ksagnowski@gmail.com>
2019-01-14 16:27:36 +0000

    Clean up reactivatePlaybackSlidersCallback.m

--- reactivatePlaybackSlidersCallback.m
+++ reactivatePlaybackSlidersCallback.m
@@ -1,4 +1,7 @@
 function reactivatePlaybackSlidersCallback(player, ~)
+    % reactivatePlaybackSlidersCallback Reactivates playback setting sliders in the "Results" tab of the app.
+    %   This function is called by the timer of MATLAB's audioplayer object once it stops.
+
     player.UserData.RecordingVolumeSlider.Enable = true;
     player.UserData.MetronomeVolumeSlider.Enable = true;
     player.UserData.PlaybackRateSlider.Enable = true;
