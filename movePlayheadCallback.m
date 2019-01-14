beedf1690b2ff47951d4c40377c10eb9fba046e4
Kacper Sagnowski <ksagnowski@gmail.com>
2019-01-14 13:43:23 +0000

    Clean up movePlayheadCallback.m

--- movePlayheadCallback.m
+++ movePlayheadCallback.m
@@ -1,4 +1,7 @@
 function movePlayheadCallback(player, ~)
+    % movePlayheadCallback Updates playhead position on the "Results" tab of the app.
+    %   This function is called every 1/8 second by the timer of MATLAB's audioplayer object.
+    
     playheadLoc = player.UserData.session.resultsPlot.playheadLoc;
     increment = player.SampleRate / 8;
     player.UserData.session.resultsPlot.movePlayhead(playheadLoc + increment);
