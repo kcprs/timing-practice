33e4728682209033ed116d3e93736d42ba44587f
Kacper Sagnowski <ksagnowski@gmail.com>
2019-01-14 13:49:06 +0000

    Clean up OnsetInfo.m

--- OnsetInfo.m
+++ OnsetInfo.m
@@ -1,23 +1,28 @@
 classdef OnsetInfo < handle
+    % OnsetInfo Class that holds timing information of a single onset
 
     properties
-        loc;
-        prevTick;
-        nextTick;
-        closestTick;
-        value;
-        timing;
-        tolerance;
+        loc;            % Location of the onset in samples
+        prevTick;       % Location of the preceding metronome tick in samples
+        nextTick;       % Location of the following metronome tick in samples
+        closestTick;    % Location of the closest metronome tick in samples
+        value;          % Distance from onset location to the closest metronome tick
+        timing;         % Timing verdict for the onset. Values: 'Early', 'OK', 'Late'
+        tolerance;      % Maximum distance from the closest metronome
+                        % tick for the onset's timing to be considered 'OK'
     end
 
     methods
 
         function self = OnsetInfo(loc, prevTick, nextTick, tolerance)
+            % OnsetInfo Constructor for the OnsetInfo class
+
             self.loc = loc;
             self.prevTick = prevTick;
             self.nextTick = nextTick;
             self.tolerance = tolerance;
 
+            % Decide on the timing verdict
             if (loc - prevTick < nextTick - loc || nextTick == 0) && prevTick ~= 0
                 self.closestTick = prevTick;
                 self.timing = 'Late';
