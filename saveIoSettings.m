42018867df89f48821b81b3b2c6ee4447781785c
Kacper Sagnowski <ksagnowski@gmail.com>
2019-01-14 17:02:16 +0000

    Clean up saveIoSettings.m

--- saveIoSettings.m
+++ saveIoSettings.m
@@ -1,6 +1,7 @@
 function saveIoSettings(app)
+    % saveIoSettings Saves I/O settings of the app to a file for later use
+
     inputDevice = app.InputDeviceDropDown.Value;
     outputDevice = app.OutputDeviceDropDown.Value;
-
     save('resources/IOSettings.mat', 'inputDevice', 'outputDevice');
 end
